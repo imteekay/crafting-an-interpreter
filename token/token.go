befcd36e9e0240c9b52b65547603357ba11de7f5
= <leandrotk100@gmail.com>
2020-12-30 18:56:21 -0300

    Building tokens

---
+++ token/token.go
@@ -0,0 +1,34 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF 		= "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT 	= "INT" // 1343456
+	
+	// Operators
+	ASSIGN = "="
+	PLUS	 = "+"
+	
+	// Delimiters
+	COMMA		  = ","
+	SEMICOLON = ";"
+
+	LPAREN 		= "("
+	RPAREN 		= ")"
+	LBRACE 		= "{"
+	RBRACE 		= "}"
+	
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET 		 = "LET"
+)
