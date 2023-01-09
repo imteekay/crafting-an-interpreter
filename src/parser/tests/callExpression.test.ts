eb952adf3b84cdf1f6c3357ee4da56781ea2070a
TK <leandrotk100@gmail.com>
2023-01-09 17:04:54 -0300

    *

--- src/parser/tests/callExpression.test.ts
+++ src/parser/tests/callExpression.test.ts
@@ -1,10 +1,8 @@
 import { describe, it, expect } from 'vitest';
 import {
-  BlockStatement,
   ExpressionStatement,
   Identifier,
   InfixExpression,
-  FunctionLiteral,
   CallExpression,
   IntegerLiteral,
 } from 'ast';
